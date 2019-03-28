bdb2c8cd50b74bb2d7e9d17ef45d28ce9429ba40
Praveenkumarm373 <45892375+Praveenkumarm373@users.noreply.github.com>
2019-03-28 12:15:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello ZippyOps')
+  response.send('Hello Zippy')
 })
 
 app.listen(app.get('port'), function() {
