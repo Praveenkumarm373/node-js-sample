ecc479596bf87d6b2a49c70bfed9fee890908010
Praveenkumarm373 <45892375+Praveenkumarm373@users.noreply.github.com>
2019-04-04 06:01:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello praveen')
+  response.send('Hello kumar')
 })
 
 app.listen(app.get('port'), function() {
